b519fc885532f4c896258b10bcc100ef18251115
SpasyonovR <spasyonov@mail.ru>
2017-03-21 17:59:42 +0200

    Урок 3 - создание класса.

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,7 +10,19 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            int x1 = 1;
+            Point p1=new Point();
+            p1.x = 1;
+            p1.y = 3;
+            p1.symbol = '*';
+            p1.Draw();
+
+            Point p2 = new Point();
+            p2.x = 5;
+            p2.y = 8;
+            p2.symbol = '#';
+            p2.Draw();
+           
+            /*int x1 = 1;
             int y1 = 3;
             char symbol1 = '*';
 
@@ -20,15 +32,15 @@ namespace Snake
             int y2 = 8;
             char symbol2 = '#';
 
-            Draw(x2, y2, symbol2);
-            
+            Draw(x2, y2, symbol2);*/
+
             Console.ReadLine();
         }
 
-        static void Draw(int x, int y, char symbol)
+        /*static void Draw(int x, int y, char symbol)
         {
             Console.SetCursorPosition(x, y);
             Console.Write(symbol);
-        }
+        }*/
     }
 }
