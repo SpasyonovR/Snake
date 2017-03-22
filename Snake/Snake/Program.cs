3b614103bd3eaa5b7ded88b8a2268fec72f47918
SpasyonovR <spasyonov@mail.ru>
2017-03-22 18:17:22 +0200

    Осваиваем класс "Список".

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,12 +10,38 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1=new Point(1,3, '*');
-            p1.Draw();
+            List<int> numList = new List<int>();
+            numList.Add(20);
+            numList.Add(21);
+            numList.Add(22);
 
-            Point p2 = new Point(5,8, '#');
-            p2.Draw();
-           
+            int n0 = numList[0];
+            int n1 = numList[1];
+            int n2 = numList[2];
+            Console.WriteLine("n0=" + n0 + ", n1=" + n1 + ", n2=" + n2);
+
+            numList.RemoveAt(1);
+            
+            foreach (int i in numList)
+            {
+                Console.WriteLine(i);
+            }
+
+            Point p1 = new Point(1, 3, '*');
+            Point p2 = new Point(5, 8, '#');
+            Point p3 = new Point(2, 4, '%');
+            Point p4 = new Point(15, 8, '$');
+
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
+            pList.Add(p3);
+            pList.Add(p4);
+            for(int i=0; i<4; i++)
+            {
+                pList[i].Draw();
+            }
+
             Console.ReadLine();
         }
     }
